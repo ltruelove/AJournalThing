6bc4d84d07ac829640959139acfc4d755b405aeb
ltruelove <levi@levitruelove.com>
2018-01-27 22:26:44 -0500

    Making the UI a little more interfacy.

--- editPost.js
+++ editPost.js
@@ -1,4 +1,29 @@
-var app = angular.module("editPost", ['textAngular']); 
-app.controller("postController", function($scope, textAngularManager) {
+var app = angular.module("editPost", ['textAngular', 'ngFileUpload']); 
+app.controller("postController", function($scope, textAngularManager, Upload) {
+    $scope.postId = 0;
+    $scope.postTitle = "Test Title";
     $scope.postContent = "<h1>Test</h1>";
+    $scope.postFiles = [];
+
+    $scope.uploadFiles = function(files){
+        for(var i = 0; i < files.length; i++){
+            var file = files[i];
+            var exists = false;
+
+            for(var n = 0; n < $scope.postFiles.length; n++){
+                var postFile = $scope.postFiles[n];
+
+                if(postFile.name === file.name
+                    && postFile.size === file.size && postFile.lastModified === file.lastModified){
+                    exists = true;
+                }
+            }
+
+            if(!exists){
+                $scope.postFiles.push(file);
+            }
+
+        }
+        console.log($scope.postFiles);
+    };
 });
